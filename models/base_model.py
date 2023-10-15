07df45670cb17d2480690acfb9a0344e464c4b8a
10-menachi <bobitlmr188221@spu.ac.ke>
2023-10-15 03:38:59 +0300

    Update base_models class

--- models/base_model.py
+++ models/base_model.py
@@ -30,25 +30,19 @@ class BaseModel:
             *args: Unused
             **kwargs: Dictionary containing attribute names and values
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
 
         if kwargs:
             for key, value in kwargs.items():
                 if key != '__class__':
-                    if key == 'created_at' or key == 'updated_at':
-                        setattr(self, key, datetime.strptime(
-                            value, '%Y-%m-%dT%H:%M:%S.%f'))
-                    else:
-                        setattr(self, key, value)
-
-            if 'id' not in kwargs:
-                self.id = str(uuid.uuid4())
-            if 'created_at' not in kwargs:
-                self.created_at = datetime.now()
-            if 'updated_at' not in kwargs:
-                self.updated_at = datetime.now()
+                    setattr(self, key, value)
+            self.created_at = datetime.strptime(
+                self.created_at, "%Y-%m-%dT%H:%M:%S.%f")
+            self.updated_at = datetime.strptime(
+                self.updated_at, "%Y-%m-%dT%H:%M:%S.%f")
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             storage.new(self)
 
     def __str__(self):
@@ -71,7 +65,7 @@ class BaseModel:
         Returns a dictionary containing all keys/values
         of the __dict__ of the instance
         """
-        new_dict = self.__dict__.copy()
+        new_dict = vars(self).copy()
         new_dict["__class__"] = self.__class__.__name__
         new_dict["created_at"] = self.created_at.isoformat()
         new_dict["updated_at"] = self.updated_at.isoformat()
